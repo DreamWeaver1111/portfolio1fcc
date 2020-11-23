1d1c286db0a09d73b2ee9c0c3a84fad32c81dd9b
Sarah Slate <52806370+DreamWeaver1111@users.noreply.github.com>
2020-11-22 21:29:49 -0700

    portfolio1

---
+++ app.js
@@ -0,0 +1,20 @@
+// ********** smooth scroll ************
+// select links
+const scrollLinks = document.querySelectorAll(".scroll-link");
+scrollLinks.forEach(link => {
+    link.addEventListener("click", e => {
+        // prevent default
+        e.preventDefault();
+        const id = e.target.getAttribute("href").slice(1);
+        const element = document.getElementById(id);
+        //
+        let position = element.offsetTop - 49;
+
+        window.scrollTo({
+            left: 0,
+            // top: element.offsetTop,
+            top: position,
+            behavior: "smooth"
+        });
+    });
+});
